3651723a2194bf016b66dbd06f49f7e9cac4ea64
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2025-12-04 16:36:21 +0530

    Update Must_join.py

--- PRITI_CHATBOT/modules/Must_join.py
+++ PRITI_CHATBOT/modules/Must_join.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from PRITI_CHATBOT import PRITI_CHATBOT as app
-from config import UPDATE_CHNL as MUST_JOIN
+from config import SUPPORT_GRP as MUST_JOIN
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
